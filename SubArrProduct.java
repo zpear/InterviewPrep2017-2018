998d8e40cc4e30d051be83cc6a8058239fb40f12
Zac Pear <zpear@me.com>
2018-02-17 16:22:49 -0500

    Good time complexity solution for finding num contiguous subarrs w/ product < k

--- SubArrProduct.java
+++ SubArrProduct.java
@@ -1,7 +1,35 @@
+
+/*
+ * Your are given an array of positive integers nums.
+ * Count and print the number of (contiguous) subarrays
+ * where the product of all the elements in the subarray 
+ * is less than k.
+ */
 import java.util.LinkedList;
 
 public class SubArrProduct {
-  
+
+  public int goodTimeComplexity(int[] nums, int k) {
+    int numSums = 0;
+    int left = 0;
+    int right = 0;
+    int currentSum = 1;
+
+    while (right < nums.length) {
+      // Resize window if needed
+      currentSum *= nums[right];
+      while (currentSum >= k && left <= right) {
+        currentSum /= nums[left];
+        left++;
+      }
+      if (currentSum < k) {
+        numSums += right - left + 1;
+      }
+      right++;
+    }
+    return numSums;
+  }
+
   public int badTimeComplexity(int[] nums, int k) {
     LinkedList<Integer>[] sums = new LinkedList[nums.length];
     int sumCount = 0;
@@ -30,4 +58,9 @@ public class SubArrProduct {
     return sumCount;
   }
 
+  public static void main(String[] args) {
+    SubArrProduct test = new SubArrProduct();
+    int[] tArr = {1,1,1};
+    System.out.println(test.goodTimeComplexity(tArr, 2));
+  }
 }
